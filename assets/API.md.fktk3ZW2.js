2632f308c4d43c379433805db8c60283f91f92f0
Leo Nicolle <leo@linkurio.us>
2023-12-06 18:45:10 +0100

    Updates

---
+++ assets/API.md.fktk3ZW2.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as a}from"./chunks/framework.RPs0iv3U.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"API.md","filePath":"API.md"}'),r={name:"API.md"};function o(s,c,n,p,_,d){return t(),a("div")}const f=e(r,[["render",o]]);export{m as __pageData,f as default};

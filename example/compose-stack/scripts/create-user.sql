74614c4c76990223d184bbeb2c13a2fdf52a83a5
Leo Nicolle <leo@linkurio.us>
2024-01-16 11:46:45 +0100

    readd function creations

--- example/compose-stack/scripts/create-user.sql
+++ example/compose-stack/scripts/create-user.sql
@@ -1,5 +1,5 @@
 alter session set container = FREEPDB1;
-drop user &1;
+drop user &1 cascade;
 create user &1 identified by &2;
 grant resource, connect to &1;
 alter user &1 quota unlimited on users;

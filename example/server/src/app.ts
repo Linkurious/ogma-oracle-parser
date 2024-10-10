2503337955a8995f34460901ec682ed43ddd3961
Leo Nicolle <leo@linkurio.us>
2024-10-10 15:38:54 +0200

    update deps and cleanup script

--- example/server/src/app.ts
+++ example/server/src/app.ts
@@ -82,8 +82,8 @@ export default function createApp() {
           )`;
         return getRawGraph({ query, conn }).then((r) => res.json(r));
       });
-      app.get("/edges/:type/:pageStart/:pageLength", (req, res) => {
-        const { type, pageStart, pageLength } = req.params;
+      app.get("/edges/:type/:pageStart/:maxResults", (req, res) => {
+        const { type, pageStart, maxResults } = req.params;
         const query = `SELECT e
           FROM graph_table (
             openflights_graph
@@ -92,13 +92,16 @@ export default function createApp() {
               EDGE_ID(e1) AS e
             )
           )
-          OFFSET ${pageStart} ROWS FETCH NEXT ${pageLength} ROWS ONLY`;
+          OFFSET ${pageStart} ROWS FETCH NEXT ${maxResults} ROWS ONLY`;
         return getRawGraph({
           query,
           conn,
+          pageStart: 0,
+          maxResults: Number(maxResults),
         }).then((r) => res.json(r));
       });
       app.get("/nodes/:type", (req, res) => {
+        const maxResults = 300;
         const query = `select v
           from graph_table (
             openflights_graph
@@ -106,7 +109,9 @@ export default function createApp() {
             columns (
               VERTEX_ID(v1) as v
             )
-          )`;
+          )
+            OFFSET 0 ROWS FETCH NEXT ${maxResults} ROWS ONLY  
+          `;
         return getRawGraph({ query, conn, maxResults: 300 }).then((r) =>
           res.json(r)
         );
